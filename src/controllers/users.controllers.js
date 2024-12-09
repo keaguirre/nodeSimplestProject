ac0c64d3bc29097c526d21ace17b51ff89bdc749
keaguirre <ke.aguirre@duocuc.cl>
2024-12-09 03:38:06 -0300

    db ops from users.routes to users.controllers

--- src/controllers/users.controllers.js
+++ src/controllers/users.controllers.js
@@ -1,5 +1,12 @@
 import { pool } from '../db.js';
 
+// router.get('/users', async (req, res) => {
+//     await pool.query('SELECT * FROM users', () => {
+//         res.status(200).json(results.rows);
+//     });
+//     res.send('Obteniendo usuarios');
+// });
+
 export const getAllUsers = async (req, res) => {
     try{
         const { rows } = await pool.query('SELECT * FROM users');
@@ -27,3 +34,35 @@ export const getUserById = async (req, res) => {
         res.status(500).json({message: 'Error al obtener el usuario'});
     }
 }
+
+export const createUser = async (req, res) => {
+    const data = req.body;
+    try{
+        const { rows } = await pool.query('INSERT INTO users (name, email) VALUES ($1, $2) RETURNING *', [data.name, data.email]);
+        return rows.length > 0 ? res.json(rows[0]) : res.status(400).json({message: 'Error al insertar el usuario'});
+    }catch(error){
+        res.status(400).json({message: 'Error al insertar el usuario'});
+    }
+}
+
+export const updateUser = async (req, res) => {
+    const { id } = req.params;
+    const { data } = req.body;
+    try{
+        const { rows } = await pool.query('UPDATE users SET name = $1, email = $2 WHERE id = $3 RETURNING *', [data.name, data.email, id]);
+        return res.json(rows[0]);
+    }catch{
+        res.status(400).json({message: 'Error al actualizar el usuario'});
+    }
+    res.send('Actualizar usuarios con id y params'+ id);
+}
+
+export const deleteUser = async (req, res) => {
+    const { userId } = req.params;
+    const { rowCount } = await pool.query('DELETE FROM users WHERE id = $1 RETURNING *', [userId]);
+    if(rowCount.length === 0){
+        res.status(404).json({message: 'No se encontro el usuario solicitado'});
+    }else if(rowCount.length === 1){
+        res.status(204);
+    }
+};
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,6 +1,6 @@
 import  { Router } from 'express';
 import { pool } from '../db.js';
-import { getAllUsers, getUserById } from '../controllers/users.controllers.js';
+import { createUser, deleteUser, getAllUsers, getUserById, updateUser } from '../controllers/users.controllers.js';
 
 const router = Router();
 
@@ -8,46 +8,14 @@ router.get('/', (_, res) => {
     res.status(200).json({message: 'Ruta base API'});
 });
 
-// router.get('/users', async (req, res) => {
-//     await pool.query('SELECT * FROM users', () => {
-//         res.status(200).json(results.rows);
-//     });
-//     res.send('Obteniendo usuarios');
-// });
 router.get('/users', getAllUsers);
 
 router.get('/users/:id', getUserById);
 
-router.post('/users', async (req, res) => {
-    const data = req.body;
-    try{
-        const { rows } = await pool.query('INSERT INTO users (name, email) VALUES ($1, $2) RETURNING *', [data.name, data.email]);
-        return rows.length > 0 ? res.json(rows[0]) : res.status(400).json({message: 'Error al insertar el usuario'});
-    }catch(error){
-        res.status(400).json({message: 'Error al insertar el usuario'});
-    }
-});
-router.delete('/users/:id', async(req, res) => {
+router.post('/users', createUser);
 
-    const { userId } = req.params;
-    const { rowCount } = await pool.query('DELETE FROM users WHERE id = $1 RETURNING *', [userId]);
-    if(rowCount.length === 0){
-        res.status(404).json({message: 'No se encontro el usuario solicitado'});
-    }else if(rowCount.length === 1){
-        res.status(204);
-    }
-});
+router.put('/users/:id', updateUser);
 
-router.put('/users/:id', async (req, res) => {
-    const { id } = req.params;
-    const { data } = req.body;
-    try{
-        const { rows } = await pool.query('UPDATE users SET name = $1, email = $2 WHERE id = $3 RETURNING *', [data.name, data.email, id]);
-        return res.json(rows[0]);
-    }catch{
-        res.status(400).json({message: 'Error al actualizar el usuario'});
-    }
-    res.send('Actualizar usuarios con id y params'+ id);
-});
+router.delete('/users/:id', deleteUser);
 
 export default router;
